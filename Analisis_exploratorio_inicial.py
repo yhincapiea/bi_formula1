15d74b2429197ad0f739f126d680a9163d38f266
yhincapiea <yhincapiea>
2024-10-26 13:31:07 +0000

    prueba3

--- Analisis_exploratorio_inicial.py
+++ Analisis_exploratorio_inicial.py
@@ -35,3 +35,32 @@ dbutils.fs.ls(raw_location) #Permite buscar los archivos
 # COMMAND ----------
 
 dbutils.fs.ls(raw_location) #Permite buscar los archivos
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Ahora inspeccionemos algúna carpeta aleatoria, Data_5 por ejemplo
+
+# COMMAND ----------
+
+dbutils.fs.ls(f"{raw_location}/circuits.csv")  #Agregar la ruta de la carpeta para cargar lo que aparece en la subcarpeta
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Tomamos algún archivo aleatorio para hacer una exploración de datos
+# MAGIC
+# MAGIC
+# MAGIC Cargamos la información dentro en un dataframe de spark
+
+# COMMAND ----------
+
+df = (
+    spark
+    .read
+    .format('csv')
+    .option('header', True)
+    .load("abfss://dataengineering@bidatarepositoryg3yhinca.dfs.core.windows.net/RAW/circuits.csv") #Añadir la ruta para cargar el dataframe de unos de los archivos 
+)
+
+display(df)
